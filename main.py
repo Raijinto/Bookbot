b3ad1550bd1e147bc17c479dce6be809799ee502
veteran_scrub <brianc236236@gmail.com>
2024-07-08 16:08:31 -0400

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    return len(file_contents.split())
+    book = "books/frankenstein.txt"
+    words = get_book_text(book)
+    num_words = get_num_words(words)
+    print(f"This book contains {num_words} words")
+        
+
+def get_book_text(book):
+    with open(book) as f:
+        return f.read()
+
+def get_num_words(word_count):
+    return len(word_count.split())
+
 main()
