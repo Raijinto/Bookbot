491f712116220f05e5dbd88f18bfc11859b872b8
veteran_scrub <brianc236236@gmail.com>
2024-07-09 12:58:40 -0400

    added dictionary that counts each time letter used

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book = "books/frankenstein.txt"
     words = get_book_text(book)
     num_words = get_num_words(words)
+    letter_count = get_letter_count(words)
     print(f"This book contains {num_words} words")
         
 
@@ -9,7 +10,19 @@ def get_book_text(book):
     with open(book) as f:
         return f.read()
 
-def get_num_words(word_count):
-    return len(word_count.split())
+def get_num_words(num_words):
+    return len(num_words.split())
+
+def get_letter_count(book):
+    dictionary = {}
+    lower_case = book.lower()
+    for letters in lower_case:
+        if letters in dictionary:
+            dictionary[letters] += 1
+        else:
+            dictionary[letters] = 1
+    return dictionary
+
+    
 
 main()
