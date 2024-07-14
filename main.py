9a8a20ef5643c8b1eff44d595cf78c9bb74c978a
veteran_scrub <brianc236236@gmail.com>
2024-07-14 13:21:51 -0400

    added report

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def main():
     words = get_book_text(book)
     num_words = get_num_words(words)
     letter_count = get_letter_count(words)
+    list_of_dict = convert_to_dictionaries(letter_count)
+    sorted = sort_list(list_of_dict)
+    
+    print("--- Begin report of books/frankenstein.txt ---")
     print(f"This book contains {num_words} words")
-    print(letter_count)
-        
+    for item in sorted:
+        print(f"The '{item['letter']}' letter appeared {item['num']} times")
+    print("--- End report ---")
 
 def get_book_text(book):
     with open(book) as f:
@@ -18,11 +23,27 @@ def get_letter_count(book):
     dictionary = {}
     lower_case = book.lower()
     for letters in lower_case:
-        if letters in dictionary:
-            dictionary[letters] += 1
-        else:
-            dictionary[letters] = 1
+        if letters.isalpha():
+            if letters in dictionary:
+                dictionary[letters] += 1
+            else:
+                dictionary[letters] = 1
     return dictionary
+    
+def convert_to_dictionaries(dict):
+    list_of_dict = []
+    for key in dict:
+        new_dict = {"letter": key, "num": dict[key]}
+        list_of_dict.append(new_dict)
+    return list_of_dict
+
+
+def key_sort(dict):
+    return dict["num"]
+
+def sort_list(unsorted_dict):
+    unsorted_dict.sort(reverse=True, key=key_sort)
+    return unsorted_dict
     
 
 main()
